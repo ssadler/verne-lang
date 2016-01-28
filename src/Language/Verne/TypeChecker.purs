1d80376a42bf7c5d436956a4e8d70e079c17c4e0
Scott Sadler <scott@alphanode.co>
2016-01-28 14:11:25 +0100

    clean up type checker a bit. Come back to this. on to greener pastures.

--- src/Language/Verne/TypeChecker.purs
+++ src/Language/Verne/TypeChecker.purs
@@ -9,6 +9,8 @@ module Language.Verne.TypeChecker
 
 import Prelude
 
+import Control.Monad.Reader
+
 import Data.Array ((:), head, length, replicate, uncons, zipWith)
 import Data.Array.Unsafe (tail)
 import Data.Either
@@ -42,33 +44,28 @@ type TypeGraph = { componentByName :: StrMap Component
                  , componentsByTypeHead :: StrMap (Array Component)
                  }
 
+
 -- todo: typed errors
 -- todo: component properties are extracted and verified at init time
 typeLisp :: TypeGraph -> Type -> LISP Pos Atom -> LISP_T Atom
 typeLisp graph typ' lisp = anno typ' lisp
   where
     anno :: Type -> LISP Pos Atom -> LISP_T Atom
-    anno typ (LIST pos arr) =
-        case uncons arr of
-            Nothing -> LIST_T typ pos (Just "Empty expression not allowed") []
-            Just { head: x, tail: xs } -> 
-                let atom = anno typ x
-                    typePadding = replicate (length xs) ""
-                in  case atom of
-                     -- cant check any of the list
-                     ATOM_T _ _ _ (Left err) ->
-                         LIST_T typ pos (Just err) (atom : zipWith anno typePadding xs)
-                     -- LIST_T _ _ _ _          -> LIST_T typ pos (Just "oshit") arr
-                     -- can check the list. If it's the wrong length, assign
-                     -- an erorr to the list node. Use "()" as a substitute for
-                     -- no type for now. 
-                     ATOM_T _ _ _ (Right (Component com)) ->
-                       if length com.signature -1 == length xs
-                          then let sig = tail com.signature
-                               in LIST_T typ pos Nothing (atom : zipWith anno sig xs)
-                          else let err = Just (errArity "thingy")
-                                   sig = tail (com.signature ++ typePadding)
-                               in LIST_T typ pos err (atom : zipWith anno sig xs)
+    anno typ (LIST pos arr) = case uncons arr of
+        Nothing -> LIST_T typ pos (Just "Empty expression not allowed") []
+        Just { head: x, tail: xs } ->
+            let atom = anno typ x
+                typePadding = replicate (length xs) ""
+            in  case atom of
+                 ATOM_T _ _ _ (Left err) ->
+                     LIST_T typ pos (Just err) (atom : zipWith anno typePadding xs)
+                 ATOM_T _ _ _ (Right (Component com)) ->
+                   if length com.signature -1 == length xs
+                      then let sig = tail com.signature
+                           in LIST_T typ pos Nothing (atom : zipWith anno sig xs)
+                      else let err = Just (errArity "thingy")
+                               sig = tail (com.signature ++ typePadding)
+                           in LIST_T typ pos err (atom : zipWith anno sig xs)
 
     anno typ (ATOM pos n@(Name name)) = ATOM_T typ pos n $
         case lookup name graph.componentByName of
@@ -83,7 +80,6 @@ typeLisp graph typ' lisp = anno typ' lisp
         if typ == "String"
            then Right $ Component { signature: ["String"] }
            else errExpected typ "String"
-
 
     errExpected typ t = Left $ "Couldn't match expected type " ++ typ ++ " with " ++ t
     errArity name = "Wrong number of arguments for " ++ name
